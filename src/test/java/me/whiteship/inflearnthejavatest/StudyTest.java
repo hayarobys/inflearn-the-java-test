1c50e437a7feb33c9d105c3f508338f45f515b99
hayarobys <hayarobys@gmail.com>
2020-12-22 18:34:44 +0900

    no message

--- src/test/java/me/whiteship/inflearnthejavatest/StudyTest.java
+++ src/test/java/me/whiteship/inflearnthejavatest/StudyTest.java
@@ -13,7 +13,8 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
-import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledOnOs;
 import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
@@ -21,15 +22,19 @@ import org.junit.jupiter.api.condition.EnabledOnJre;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.JRE;
 import org.junit.jupiter.api.condition.OS;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
 
-	@Test
+	@FastTest
 	@EnabledOnOs({OS.MAC, OS.LINUX})
 	@EnabledOnJre({JRE.JAVA_8, JRE.JAVA_11})
 	@EnabledIfEnvironmentVariable(named = "TEST_ENV", matches = "LOCAL")
-	@Tag("fast")
 	void create_new_study() {
 		String test_env = System.getenv("TEST_ENV");
 		assumeTrue("LOCAL".equalsIgnoreCase(test_env));
@@ -46,10 +51,9 @@ class StudyTest {
 		});
 	}
 	
-	@Test
+	@SlowTest
 	@DisplayName("스터디 다시 만들기")
 	@DisabledOnOs(OS.MAC)
-	@Tag("slow")
 	void create_new_study_again() {
 		System.out.println("Create1");
 	}
@@ -58,6 +62,20 @@ class StudyTest {
 	@Disabled
 	void create_disabled_study() {
 		System.out.println("Create2");
+	}
+	
+	@DisplayName("스터디 만들기")
+	@RepeatedTest(value=10, name="{displayName}, {currentRepetition}/{totalRepetitions}")
+	void repeatTest(RepetitionInfo repetitionInfo){
+		System.out.println("test " + repetitionInfo.getCurrentRepetition() + "/" + repetitionInfo.getTotalRepetitions());
+	}
+	
+	@DisplayName("스터디 만들기")
+	@ParameterizedTest(name = "{index} {displayName} message={0}")
+	@ValueSource(strings = {"날씨가", "많이", "추워지고", "있네요."})
+	//@NullAndEmptySource 
+	void parameterizedTest(String message){
+		System.out.println(message);
 	}
 	
 	@BeforeAll
